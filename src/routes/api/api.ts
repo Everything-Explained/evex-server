2d84a3e7378ad412a7cd851c8df8f6e1685f6df6
jaeiya <exanix@gmail.com>
2022-06-17 15:51:02 -0700

    feat(api): catch all unspecified `/api` routes using wildcard

--- src/routes/api/api.ts
+++ src/routes/api/api.ts
@@ -1,6 +1,5 @@
 
-import { FastifyPluginAsync, RouteShorthandOptions } from 'fastify';
-import { Type } from '@sinclair/typebox';
+import { FastifyPluginAsync } from 'fastify';
 import useAuthSetupRoute from './api-auth-setup';
 import { useAPIAuthHook } from '../../hooks/api-auth-hook';
 import useAuthRed33mRoute from './api-auth-red33m';
@@ -9,29 +8,13 @@ import useAuthRed33mRoute from './api-auth-red33m';
 
 
 
-const apiOptions: RouteShorthandOptions = {
-  schema: {
-    response: {
-      403: Type.String()
-    },
-  }
-};
-
-
 const api: FastifyPluginAsync = async (fastify) => {
   useAPIAuthHook(fastify);
   useAuthSetupRoute(fastify, '/api');
   useAuthRed33mRoute(fastify, '/api');
 
-  fastify.get('/api', apiOptions, (req, res) => {
-    res.status(403);
-    res.send('Forbidden');
-  });
-
-  fastify.get('/api/*', apiOptions, (req, res) => {
-    res.log.info('root level denial');
-    res.status(403);
-    res.send('Forbidden');
+  fastify.get('/api*', (req, res) => {
+    return res.forbidden('Suspicious Activity Detected');
   });
 };
 
