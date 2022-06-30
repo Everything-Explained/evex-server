66970953be1620888dbb9f1b971740faa855f644
jaeiya <exanix@gmail.com>
2022-06-29 18:41:42 -0700

    tdd(config): add all tests

---
+++ __test__/config.test.ts
@@ -0,0 +1,62 @@
+
+import { describe } from "riteway";
+import { paths, serverConfig } from "../src/config";
+import { pathResolve } from "../src/utils";
+
+
+
+
+
+
+describe('config', async t => {
+
+  t({
+    given: 'node env production',
+    should: 'set web path to ../web_client',
+    actual: paths('production').web,
+    expected: pathResolve('../web_client'),
+  });
+
+  t({
+    given: 'node env production',
+    should: 'set mail.toEthan to his email',
+    actual: serverConfig('production').mail.toEthan.includes('fake@test.yee'),
+    expected: false,
+  });
+
+  t({
+    given: 'node env production',
+    should: 'set auth.red33m to red33m code',
+    actual: serverConfig('production').auth.red33m == serverConfig('production').auth.testCode,
+    expected: false,
+  });
+
+  t({
+    given: 'node env development',
+    should: 'set mail.toEthan to fake email',
+    actual: serverConfig().mail.toEthan,
+    expected: 'fake@test.yee',
+  });
+
+  t({
+    given: 'node env development',
+    should: 'set auth.red33m to test code',
+    actual: serverConfig().auth.red33m,
+    expected: serverConfig().auth.testCode,
+  });
+
+  t({
+    given: 'node env development',
+    should: 'set web path to ../web-client/release/web_client',
+    actual: paths().web,
+    expected: pathResolve('../web-client/release/web_client'),
+  });
+});
+
+
+
+
+
+
+
+
