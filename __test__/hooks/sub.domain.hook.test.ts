57d588e3e0ced379c7665973c44a4bcb5be0096e
jaeiya <exanix@gmail.com>
2022-07-09 18:37:48 -0700

    tdd(sub-domain-hook): add all tests

---
+++ __test__/hooks/sub.domain.hook.test.ts
@@ -0,0 +1,110 @@
+
+
+import { readFile } from 'fs/promises';
+import { describe } from 'riteway';
+import { paths } from '../../src/config';
+import { pathResolve } from '../../src/utils';
+import { build, constAuthedHeader } from '../test.fastify';
+
+
+
+
+const subDomainPath = pathResolve(paths().subDomain, 'web-sub-meet', 'src');
+
+
+describe('Sub Domain -- onRequest() hook', async t => {
+  const app = await build();
+
+  t({
+    given: 'existing subdomain',
+    should: 'send 200 status',
+    actual: (await testHook()).statusCode,
+    expected: 200
+  });
+
+  const subDomainIndex =
+    await readFile(`${subDomainPath}/index.html`, { encoding: 'utf-8'})
+  ;
+  t({
+    given: 'existing subdomain',
+    should: 'send index',
+    actual: (await testHook()).payload,
+    expected: subDomainIndex
+  });
+
+  const clientIndex = await readFile(`${paths().client}/index.html`, { encoding: 'utf-8'});
+  t({
+    given: 'no subdomain',
+    should: 'pass to next route handler',
+    actual: (await testHook('localhost')).payload,
+    expected: clientIndex
+  });
+
+
+  t({
+    given: 'cached subdomain index request',
+    should: 'send index',
+    actual: (await testHook('t-cache.localhost')).payload,
+    expected: 'test cache'
+  });
+  t({
+    given: 'cached subdomain index request',
+    should: 'not cache index',
+    actual: (await testHook('t-cache.localhost')).headers['cache-control'],
+    expected: 'public, max-age=0'
+  });
+
+
+  t({
+    given: 'cached subdomain asset request',
+    should: 'send asset',
+    actual: (await testHook('t-cache.localhost', '/test-cache.txt')).payload,
+    expected: 'test cache txt'
+  });
+  t({
+    given: 'cached subdomain asset request',
+    should: 'cache asset',
+    actual: (await testHook('t-cache.localhost', '/test-cache.txt')).headers['cache-control'],
+    expected: 'max-age=31536000'
+  });
+
+
+  t({
+    given: 'uncached subdomain asset request',
+    should: 'send asset',
+    actual: (await testHook('t-nocache.localhost', '/test-nocache.txt')).payload,
+    expected: `test nocache txt`
+  });
+  t({
+    given: 'uncached subdomain asset request',
+    should: 'not cache asset',
+    actual: (await testHook('t-nocache.localhost', '/test-nocache.txt')).headers['cache-control'],
+    expected: 'public, max-age=0'
+  });
+
+  // Cleanup everything here
+  app.close();
+
+
+  async function testHook(hostname = 'meet.localhost', url = '/', header = constAuthedHeader) {
+    const { statusCode, payload, headers } = await app.inject(
+      { url: { pathname: url, hostname }, headers: header, }
+    );
+    return {
+      statusCode,
+      payload,
+      headers,
+    };
+  }
+
+});
+
+
+
+
+
+
+
+
+
+
--- __test__/test.fastify.ts
+++ __test__/test.fastify.ts
@@ -2,12 +2,16 @@
 import Fastify from 'fastify';
 import fp from 'fastify-plugin';
 import App from '../src/app';
+import { paths } from '../src/config';
+import { useSubDomainHook } from '../src/hooks/sub-domain-hook';
+import { pathJoin, pathResolve } from '../src/utils';
 
 export const userID = 'abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz';
 export const r3dUserID = 'zyxwvutsrqponmlkjihgfedcbazyxwvutsrqponmlkjihgfedcba';
 export const authorizedHeader = { 'authorization': `Bearer ${userID}`};
 export const constAuthedHeader = { 'authorization': `Bearer abqdefbhijklmngpqrstuvhxyoabcdeughijklmnnmqrstuvvxyz`};
 export const r3dAuthHeader = { 'authorization': `Bearer ${r3dUserID}`};
+export const mockDir = pathJoin(__dirname, '__mocks__');
 
 // Fill in this config with all the configurations
 // needed for testing the application
@@ -23,6 +27,18 @@ async function build () {
   // are exposed for testing purposes, this is
   // different from the production setup
   void app.register(fp(App), await config());
+  useSubDomainHook(app, [
+    {
+      name: 't-cache',
+      cache: true,
+      filesPath: pathJoin(mockDir, 'test-cache')
+    },
+    {
+      name: 't-nocache',
+      cache: false,
+      filesPath: pathJoin(mockDir, 'test-nocache')
+    },
+  ]);
 
   await app.ready();
 
